52e599c1a16fbedd7fb8b256960b9242c60bb3f4
mkusaka <hinoshita1992@gmail.com>
2019-04-21 13:21:52 +0900

    add getArticles by wrapping pocket-api npm package

---
+++ src/index.ts
@@ -0,0 +1,161 @@
+const pocket = require("pocket-api");
+
+interface requestTokenResponse {
+  code: string;
+  state: any;
+}
+
+interface responseArticle {
+  item_id: string;
+  resolved_id: string;
+  given_url: string;
+  given_title: string;
+  favorite: string;
+  status: string;
+  time_added: string;
+  time_updated: string;
+  time_read: string;
+  time_favorited: string;
+  sort_id: 10,
+  resolved_title: string;
+  resolved_url: string;
+  excerpt: string;
+  is_article: string;
+  is_index: string;
+  has_video: string;
+  has_image: string;
+  word_count: string;
+  lang: string;
+  top_image_url: string;
+  domain_metadata?: {
+    name: string;
+    logo: string;
+    greyscale_logo: string;
+  },
+  listen_duration_estimate: 835
+}
+
+/*
+{
+  '2565750566':
+  {
+    item_id: '2565750566',
+    resolved_id: '2565750566',
+    given_url: 'https://medium.com/@shintaroshiba/%E3%82%BD%E3%83%95%E3%83%88%E3%82%A6%E3%82%A7%E3%82%A2%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%E3%81%A8%E3%81%97%E3%81%A6%E3%81%AE%E3%83%88%E3%83%A8%E3%82%BF%E3%81%A8%E3%81%84%E3%81%86%E9%81%B8%E6%8A%9E-3a4ccbe4570b',
+    given_title: '',
+    favorite: '0',
+    status: '0',
+    time_added: '1555765608',
+    time_updated: '1555765610',
+    time_read: '0',
+    time_favorited: '0',
+    sort_id: 10,
+    resolved_title: 'ソフトウェアエンジニアとしてのトヨタという選択',
+    resolved_url: 'https://medium.com/@shintaroshiba/%E3%82%BD%E3%83%95%E3%83%88%E3%82%A6%E3%82%A7%E3%82%A2%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%E3%81%A8%E3%81%97%E3%81%A6%E3%81%AE%E3%83%88%E3%83%A8%E3%82%BF%E3%81%A8%E3%81%84%E3%81%86%E9%81%B8%E6%8A%9E-3a4ccbe4570b',
+    excerpt: '先日、他社からのオファーを断った。非常にエキサイティングな海外の会社（勤務地も海外）だったのだけれども、その決断に際して3週間くらいうじうじと迷っていた。',
+    is_article: '1',
+    is_index: '0',
+    has_video: '0',
+    has_image: '1',
+    word_count: '2158',
+    lang: 'ja',
+    top_image_url: 'https://cdn-images-1.medium.com/max/1200/1*dvr9fFP0Yna6HqzN9BzCyg.png',
+    domain_metadata: {
+      name: 'Medium',
+      logo: 'https://logo.clearbit.com/medium.com?size=800',
+      greyscale_logo: 'https://logo.clearbit.com/medium.com?size=800&greyscale=true'
+    },
+    listen_duration_estimate: 835
+  },
+  ....
+}
+*/
+
+interface getArticlesResponse {
+  state: number;
+  complete: number;
+  list: {
+    [key: string]: responseArticle
+  };
+  error: Error;
+  search_meta: {
+    search_type: string
+  };
+  since: number
+}
+
+class ApiClient {
+  requestTokenPath: string = "oauth/request";
+  accessTokenPath: string = "oauth/authorize";
+  getArticlesPath: string = "get";
+  addArticlesPath: string = "add";
+  sendArticlesPath: string = "send";
+  _resArticles: any;
+
+  _consumerKey: string;
+  _accessToken?: string;
+  _requestToken?: string;
+
+  constructor(
+    consumerKey: string,
+    accessToken?: string,
+    requestToken?: string,
+  ) {
+    this._consumerKey = consumerKey;
+    this._requestToken = requestToken;
+    this._accessToken = accessToken;
+  }
+
+  getRequestToken() {
+    return new Promise(resolve => {
+      pocket.getRequestToken(this._consumerKey, function(
+        data: requestTokenResponse
+      ) {
+        console.log(data.code);
+        return resolve(data.code);
+      });
+    });
+  }
+
+  getAccessToken() {
+    if (!this._requestToken) {
+      throw TypeError("this operation require request_token.")
+    }
+    return new Promise(resolve => {
+      pocket.getAccessToken(
+        this._consumerKey,
+        this._requestToken,
+        (data: requestTokenResponse) => {
+          const accessToken = data.code;
+          this._accessToken = accessToken;
+          return resolve(accessToken);
+        }
+      );
+    });
+  }
+
+  getArticles() {
+    return new Promise((resolve, reject) => {
+      if (!this._accessToken) {
+        throw TypeError("this operation require access_token.")
+      }
+      pocket.getArticles(
+        this._consumerKey,
+        this._accessToken,
+        (error, data?: getArticlesResponse) => {
+          if (error || !data) {
+            return reject(error);
+          }
+
+          const articleList = data.list;
+          this._resArticles = Object.keys(articleList).map(
+            key => articleList[key]
+          );
+          return resolve(this._resArticles);
+        }
+      );
+    });
+  }
+}
+
+export default ApiClient;
